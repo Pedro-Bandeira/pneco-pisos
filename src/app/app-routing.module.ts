340a429e4baf28ceeb1c99a3abee23b0f3218ecb
Pedro-Bandeira <pedroh.bandeira1@gmail.com>
2021-07-17 16:12:11 -0300

    atualizando pages site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'sobre', component: SobreComponent}
+  {path: 'sobre', component: SobreComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'produto', component: ProdutoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ProdutoComponent } from './pages/produto/produto.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SobreComponent
+    SobreComponent,
+    DashboardComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +34,12 @@ import { SobreComponent } from './pages/sobre/sobre.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    LayoutModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
